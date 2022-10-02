475e0c25d64c60a8de634ae8b18bf79605b4a775
eleonorarocchi <rocchi.eleonora@gmail.com>
2022-10-02 15:54:29 +0200

    aggiunta lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h1>
-      Welcome to {{ title }}!
-    </h1>  
-    <app-input-button-unit></app-input-button-unit>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'todo-list';
+  todoList = [
+    {title: 'install NodeJS'},
+    {title: 'install Angular CLI'},
+    {title: 'create new app'},
+    {title: 'serve app'},
+    {title: 'develop app'},
+    {title: 'deploy app'},
+  ];
 }
