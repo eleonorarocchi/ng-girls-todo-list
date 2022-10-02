3090eb4afd38380bef23b70c26195046207420cf
eleonorarocchi <rocchi.eleonora@gmail.com>
2022-10-02 15:40:55 +0200

    aggiornato componente

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input-button-unit.component.scss']
 })
 export class InputButtonUnitComponent implements OnInit {
-  title = 'Hello World';
-  constructor() { }
+  title: string = 'Hello World';
 
-  ngOnInit(): void {
+  constructor() { 
+    console.log('in constructor');
+    this.changeTitle('My First Angular App');
+    console.log(this.title);
+  }
+  
+  ngOnInit(): void { 
+    this.title = 'Angular CLI Rules!';
   }
 
+  changeTitle(newTitle: string) {
+    console.log(newTitle);
+    this.title = newTitle;
+  }
 }
